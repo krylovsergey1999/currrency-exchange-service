1d45daf784809f8b3dde856264db94694807cc0f
Krylov Sergey <kssergeyru@gmail.com>
2020-02-04 22:25:33 +0300

    Настроили контроллер конвертации

--- src/main/java/ru/microservice/currrencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/ru/microservice/currrencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,15 +3,12 @@ package ru.microservice.currrencyexchangeservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.microservice.currrencyexchangeservice.domain.ExchangeValue;
 import ru.microservice.currrencyexchangeservice.service.ExchangeValueService;
-
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
